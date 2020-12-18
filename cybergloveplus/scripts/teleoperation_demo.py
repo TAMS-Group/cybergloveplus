9aa4d65889cc4876351585bae35a50fb715a4adb
Hongzhuo Liang <lianghongzhuo@126.com>
2020-12-18 20:59:48 +0100

    shadow demo

--- cybergloveplus/scripts/teleoperation_demo.py
+++ cybergloveplus/scripts/teleoperation_demo.py
@@ -1,13 +1,14 @@
-"""
-move shadow to specified pose which from my cmp2 network
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import moveit_commander
 from moveit_msgs.msg import DisplayRobotState
 import rospy
-from shadow_teleop.srv import *
+from cybergloveplus.srv import CheckSelfCollision
 
-mgi = moveit_commander.MoveGroupCommander("right_hand")
+"""
+move shadow to specified pose which from cyberglove
+"""
 
 
 def clip(x, maxv=None, minv=None):
@@ -19,47 +20,56 @@ def clip(x, maxv=None, minv=None):
 
 
 def main():
-    rospy.init_node("shadow_human_dataset")
+    rospy.init_node("shadow_human_teleoperate")
+    mgi = moveit_commander.MoveGroupCommander("right_hand")
     while not rospy.is_shutdown():
         msg = rospy.wait_for_message("/hand/robot_state", DisplayRobotState)
         joints = msg.state.joint_state.position
         # shadow hand with biotac
-        feature = [joints[23], joints[22], joints[3], joints[2], joints[1],
-                   0.3498509706185152, joints[8], joints[7], joints[6], joints[5],
-                   0.3498509706185152, joints[12], joints[11], joints[10], 0.3498509706185152,
-                   joints[16], joints[15], joints[14], 0.3498509706185152, joints[21],
-                   joints[20], joints[19], joints[18], 0.3498509706185152]
+        joint = [0, 0, joints[3], joints[2], joints[1],
+                 0.3498509706185152, joints[8], joints[7], joints[6], joints[5],
+                 0.3498509706185152, joints[12], joints[11], joints[10], 0.3498509706185152,
+                 joints[16], joints[15], joints[14], 0.3498509706185152, joints[21],
+                 joints[20], joints[19], joints[18], 0.3498509706185152]
 
         # clip joints into executable ranges
-        feature[0] = clip(feature[0], 1.57, 0)
-        feature[1] = clip(feature[1], 1.57, 0)
-        feature[2] = clip(feature[2], 1.57, 0)
-        feature[4] = clip(feature[4], 1.57, 0)
-        feature[5] = clip(feature[5], 1.57, 0)
-        feature[6] = clip(feature[6], 1.57, 0)
-        feature[9] = clip(feature[9], 1.57, 0)
-        feature[10] = clip(feature[10], 1.57, 0)
-        feature[11] = clip(feature[11], 1.57, 0)
-        feature[13] = clip(feature[13], 1.57, 0)
-        feature[14] = clip(feature[14], 1.57, 0)
-        feature[15] = clip(feature[15], 1.57, 0)
-        feature[3] = clip(feature[3], 0.349, -0.349)
-        feature[7] = clip(feature[7], 0.349, -0.349)
-        feature[12] = clip(feature[12], 0.349, -0.349)
-        feature[16] = clip(feature[16], 0.349, -0.349)
-        feature[8] = clip(feature[8], 0.785, 0)
-        feature[18] = clip(feature[18], 0.524, -0.524)
-        feature[19] = clip(feature[19], 0.209, -0.209)
-        feature[20] = clip(feature[20], 1.222, 0)
-        feature[21] = clip(feature[21], 1.047, -1.047)
+        joint[0] = clip(joint[0], 1.57, 0)
+        joint[1] = clip(joint[1], 1.57, 0)
+        joint[2] = clip(joint[2], 0.349, -0.349)
+        joint[3] = clip(joint[3], 1.57, 0)
+        joint[4] = clip(joint[4], 1.57, 0)
+        joint[5] = clip(joint[5], 1.57, 0)
 
-        goal = feature
+        joint[6] = clip(joint[6], 0.785, 0)
+
+        joint[7] = clip(joint[7], 0.349, -0.349)
+        joint[8] = clip(joint[8], 1.57, 0)
+        joint[9] = clip(joint[9], 1.57, 0)
+        joint[10] = clip(joint[10], 1.57, 0)
+
+        joint[11] = clip(joint[11], 0.349, -0.349)
+        joint[12] = clip(joint[12], 1.57, 0)
+        joint[13] = clip(joint[13], 1.57, 0)
+        joint[14] = clip(joint[14], 1.57, 0)
+
+        joint[15] = clip(joint[15], 0.349, -0.349)
+        joint[16] = clip(joint[16], 1.57, 0)
+        joint[17] = clip(joint[17], 1.57, 0)
+        joint[18] = clip(joint[18], 1.57, 0)
+
+        joint[19] = clip(joint[19], 1.047, -1.047)
+        joint[20] = clip(joint[20], 1.222, 0)
+        joint[21] = clip(joint[21], 0.209, -0.209)
+        joint[22] = clip(joint[22], 0.524, -0.524)
+        joint[23] = clip(joint[23], 1.57, 0)
+
         start = mgi.get_current_joint_values()
 
         # collision check and manipulate
-        csl_client = rospy.ServiceProxy("CheckSelfCollision", checkSelfCollision)
+        csl_client = rospy.ServiceProxy("CheckSelfCollision", CheckSelfCollision)
         try:
-            shadow_pos = csl_client(start, tuple(goal))
+            # mgi.go(joint)
+            shadow_pos = csl_client(start, tuple(joint))
             if shadow_pos.result:
                 rospy.loginfo("Move Done!")
             else:
